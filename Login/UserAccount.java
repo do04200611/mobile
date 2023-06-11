e008cc50e8f34a9cd744f7371e5cf2c79c3532c7
kimganghyeon <kimganghyeon6080@naver.com>
2023-06-11 21:05:55 +0900

    Add files via upload

---
+++ Login/UserAccount.java
@@ -0,0 +1,39 @@
+package com.sentila.log;
+
+public class UserAccount {
+    private  String idToken; //Firebase Uid(고유 토큰정보)
+    private String emailId;
+    private String password;
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public UserAccount() {}
+
+
+
+}
